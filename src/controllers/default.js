c3cc048b546f42f35eeb0d6865bc98874dfc5e7c
Gerardo Enrique <geraenrique97@hotmail.com>
2020-10-13 18:37:23 -0300

    Fixing babel config and default controller

--- src/controllers/default.js
+++ src/controllers/default.js
@@ -2,6 +2,6 @@
  * Controller for check status of service
  * @param  {object} res
  */
-export const healthCheckController =  (res) => {
+export const healthCheckController =  (_, res) => {
   res.status(200).json({message: 'UP'});
 };
